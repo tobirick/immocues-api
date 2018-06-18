c681d021317b37b8af7f2c04e2d8132cd99e7569
tobirick <tobirickmann@gmail.com>
2018-06-18 09:04:33 +0200

    add new customer and user fields to mongoose schema

--- api/models/customer.js
+++ api/models/customer.js
@@ -1,52 +1,60 @@
 const mongoose = require("mongoose");
 
-const customerSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  firstName: {
-    type: String,
-    required: true
+const customerSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    profileImageSrc: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    company: {
+      type: String
+    },
+    eMail: {
+      type: String,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    phoneNumber: {
+      type: String
+    },
+    mobilePhoneNumber: {
+      type: String
+    },
+    websiteUrl: {
+      type: String
+    },
+    addressCity: {
+      type: String
+    },
+    addressStreet: {
+      type: String
+    },
+    addressLatLng: {
+      type: Object
+    },
+    notes: {
+      type: String
+    },
+    tags: {
+      type: [String]
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
   },
-  lastName: {
-    type: String,
-    required: true
-  },
-  profileImageSrc: {
-    type: String
-  },
-  title: {
-    type: String
-  },
-  company: {
-    type: String
-  },
-  eMail: {
-    type: String,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-  },
-  phoneNumber: {
-    type: String
-  },
-  mobilePhoneNumber: {
-    type: String
-  },
-  websiteUrl: {
-    type: String
-  },
-  addressCity: {
-    type: String
-  },
-  addressStreet: {
-    type: String
-  },
-  addressLatLng: {
-    type: Object
-  },
-  notes: {
-    type: String
-  },
-  tags: {
-    type: [String]
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Customer", customerSchema);
--- api/models/user.js
+++ api/models/user.js
@@ -1,17 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String
+    },
+    phoneNumber: {
+      type: String
+    },
+    mobilePhoneNumber: {
+      type: String
+    },
+    addressCity: {
+      type: String
+    },
+    addressStreet: {
+      type: String
+    },
+    addressLatLng: {
+      type: Object
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
